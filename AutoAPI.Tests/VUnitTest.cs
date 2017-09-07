732b0ede796feb97c0e5a19df5f33cfc93a78b03
Muhammad Soliman <msuliman@mum.edu>
2017-09-07 17:04:12 -0500

    Unit Test Addition, Just a sample

---
+++ AutoAPI.Tests/VUnitTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoAPI.Controllers;
+using AutoAPI.Infrastructure.Caching;
+using AutoAPI.Models.Context;
+using AutoAPI.Models.Vehicles.Impl;
+using AutoAPI.Models.Vehicles.Model;
+using AutoAPI.Models.Vehicles.ViewModel;
+using Xunit;
+
+namespace AutoAPI.Tests
+{
+    public class VUnitTest
+    {
+        private ICache _cache;
+        private IVehicle _vehicle;
+        
+        public VUnitTest(ICache cache, IVehicle vehicle)
+        {
+            _cache = cache;
+            _vehicle = vehicle;
+        }
+        
+      
+        
+        
+        [Fact]
+        public void Get_ShouldReturnNotReturnAnythingVehicles()
+        {
+          VehiclesController vc = new VehiclesController(_vehicle, _cache);
+            var vcs = vc.Get(new VehicleSearchCriteria()
+            {
+                Year = 1900
+            }).GetAwaiter().GetResult();
+            
+            Assert.Equal(vcs.Count(), 0);
+        }
+    }
+}
