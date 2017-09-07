b9223260ba34faac298348e5e0af501f78aa9ee7
Muhammad Soliman <msuliman@mum.edu>
2017-09-07 17:04:22 -0500

    Unit Test Addition, Just a sample

---
+++ AutoAPI.Tests/TestStartup.cs
@@ -0,0 +1,26 @@
+﻿using AutoAPI.Infrastructure.Caching;
+using AutoAPI.Models.Context;
+using AutoAPI.Models.Vehicles.Impl;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AutoAPI.Tests
+{
+    public class TestStartup: Startup
+    {
+        public TestStartup(IHostingEnvironment env, ILoggerFactory loggerFactory) : base(env, loggerFactory)
+        {
+            
+        }
+        
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            services.AddSingleton<ICache, MemCache>();
+            services.AddSingleton<IContext, EContext>();
+            services.AddSingleton<IVehicle, EVehicle>();
+        }
+    }
+}
